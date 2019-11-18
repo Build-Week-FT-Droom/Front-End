d7c106d80e4e50789da6bf1e0d12ce9f81cc7253
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-18 17:27:33 -0500

    think reducers and actions are done

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -6,45 +6,107 @@ export const FETCH_USER = "FETCH_USER"
 export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS"
 export const FETCH_USER_FAIL = "FETCH_USER_FAIL"
 
+export const DELETE_USER = "DELETE_USER"
+export const DELETE_USER_FAIL = "DELETE_USER_FAIL" 
+
 export const UPDATE_USER = "UPDATE_USER"
 export const UPDATE_USER_FAIL = "UPDATE_USER_FAIL"
+
+export const FETCH_USERS = "FETCH_USERS"
+export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS"
+export const FETCH_USERS_FAIL = "FETCH_USERS_FAIL"
+
 
 export const fetchUser = () => dispatch => {
     dispatch({type: FETCH_USER});
     axiosWithAuth()
-    .get(`/api/users/${id}`)
-    .then(response => {
-      dispatch({
-        type: FETCH_USER_SUCCESS,
-        payload: response.data
-      });
-    })
-
-    .catch(error => {
-      console.log(error);
-      dispatch({
-        type: FETCH_USER_FAIL,
-        payload: error.response
-          
+        .get(`/api/auth/user/${id}`)
+        .then(response => {
+        dispatch({
+            type: FETCH_USER_SUCCESS,
+            payload: response.data
         });
-      });
-  };
+        })
 
-export const updateUser = userData => dispatch => {
+        .catch(error => {
+        console.log(error);
+        dispatch({
+            type: FETCH_USER_FAIL,
+            payload: error.response
+            
+            });
+        });
+    };
+
+////////////////////////////////////////////////////////
+//////////////// EDIT USER /////////////////////////////
+////////////////////////////////////////////////////////
+
+export const editUser = userData => dispatch => {
 
     axiosWithAuth()
-    .put(`/api/users/${userData.id}`, userData)
+        .put(`/api/auth/user/${userData.id}`, userData)
+        .then(response => {
+        dispatch({
+            type: UPDATE_USER, 
+            payload: response.data
+        });
+        })
+        .catch(error => {
+        console.log(error);
+        dispatch({
+        type: UPDATE_USER_FAIL,
+        payload: error.response
+        });
+    });
+    };
+
+/////////////////////////////////////////////////////////
+/////////////// DELETE USER   ///////////////////////////
+////////////////////////////////////////////////////////
+
+    axiosWithAuth()
+        .delete(`/api/auth/user/${userData.id}`, userData)
+        .then(response => {
+            console.log(response)
+            localStorage.removeItem('token');
+        dispatch({
+            type: DELETE_USER, 
+            payload: response.data
+        });
+        })
+        .catch(error => {
+        console.log(error);
+        dispatch({
+        type: DELETE_USER_FAIL,
+        payload: error.response
+        });
+    });
+
+
+
+
+
+/////////////////////////////////////////////////////////
+/////////////// GET ALL USERS ///////////////////////////
+////////////////////////////////////////////////////////
+
+export const fetchAllUsers = () => dispatch => {
+    dispatch({type: FETCH_USERS});
+    axiosWithAuth()
+    .get('/api/auth/users/')
     .then(response => {
       dispatch({
-        type: UPDATE_USER, 
+        type: FETCH_USERS_SUCCESS, 
         payload: response.data
       });
     })
     .catch(error => {
       console.log(error);
       dispatch({
-      type: UPDATE_USER_FAIL,
+      type: UPDATE_USERS_FAIL,
       payload: error.response
     });
   });
+
 };
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -4,63 +4,108 @@ import {
     FETCH_USER_FAIL,
 
     UPDATE_USER,
-    UPDATE_USER_FAIL
+    UPDATE_USER_FAIL,
 
     DELETE_USER,
-  
+    DELETE_USER_FAIL,
+
+    FETCH_USERS,
+    FETCH_USERS_SUCCESS,
+    FETCH_USERS_FAIL
+
   } from "./actions";
   
   const initialState = {
-    loading: false,
     user: {},
-    error: null,
-    userDeleted: false
+    users: [],
+    loading: false,
+    userDeleted: false,
+    error: null
+    
   };
   
-  export const userProfileReducer = (state = initialState, action) => {
+  export const userReducer = (state = initialState, action) => {
     switch (action.type) {
 
-      case FETCH_USER: 
-        return {
-            ...state,
-            loading: true, 
-            error: ''
-          };
+        case FETCH_USER: 
+            return {
+                ...state,
+                loading: true, 
+                error: ''
+            };
 
-      case FETCH_USER_SUCCESS: 
-        return {
-            ...state,
-            user: action.payload,
-            loading: false, 
-            error: ''
-          };
+        case FETCH_USER_SUCCESS: 
+            return {
+                ...state,
+                user: action.payload,
+                loading: false, 
+                error: ''
+            };
      
-      case FETCH_USER_FAIL: 
-        return {
-            ...state,
-            error: action.payload
-          };
+        case FETCH_USER_FAIL: 
+            return {
+                ...state,
+                error: action.payload
+            };
 
-      case UPDATE_USER:
-          return {
-            ...state,
-            user: action.payload,
-            loading: false, 
-          }
+        case UPDATE_USER:
+            return {
+                ...state,
+                user: action.payload,
+                loading: false, 
+            }
 
-      case UPDATE_USER_FAIL:
-          return {
-              ...state,
-            error: action.payload
-          }
+        case UPDATE_USER_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
 
-      case DELETE_USER: 
-        return {
-          ...state,
-          userDeleted: action.payload,
-          error: action.payload,
-          loading: action.payload
-        }
-        
+//////////////////////////////////////////////////////////////
+//////////////// DELETE USER  ////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+        case DELETE_USER: 
+            return {
+                ...state,
+                userDeleted: action.payload,
+                error: '',
+                loading: false
+            } 
+
+        case DELETE_USER_FAIL: 
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            }  
+
+///////////////////////////////////////////////////////////////
+////////////////  ALL USERS  //////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+
+        case  FETCH_USERS: 
+            return {
+                ...state,
+                loading: true, 
+                error: ''
+            };
+
+        case  FETCH_USERS_SUCCESS: 
+            return {
+                ...state,
+                users: action.payload,
+                loading: false, 
+                error: ''
+            };
+
+        case  FETCH_USERS_FAIL: 
+            return {
+                ...state,
+                error: action.payload
+            } 
+            
+
   }
 }
