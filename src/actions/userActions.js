e99b9a3bd320e9820dcaa090d0c71a6f5ca96afe
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-18 16:35:08 -0500

    working on reducer

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import React, {useState} from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-export const FETCH_USER = "FETCHING_USER"
-export const FETCH_USER_SUCCESS = "FETCHING_USER_SUCCESS"
+export const FETCH_USER = "FETCH_USER"
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS"
 export const FETCH_USER_FAIL = "FETCH_USER_FAIL"
 
 export const UPDATE_USER = "UPDATE_USER"
 export const UPDATE_USER_FAIL = "UPDATE_USER_FAIL"
 
 export const fetchUser = () => dispatch => {
-    dispatch({type: FETCHING_USER});
+    dispatch({type: FETCH_USER});
     axiosWithAuth()
     .get(`/api/users/${id}`)
     .then(response => {
@@ -24,37 +24,27 @@ export const fetchUser = () => dispatch => {
       console.log(error);
       dispatch({
         type: FETCH_USER_FAIL,
-        payload: err.response
+        payload: error.response
           
         });
       });
   };
 
 export const updateUser = userData => dispatch => {
-    dispatch({
-      type: UPDATE_USER,
-        loading: action.payload,
-        user: {},
-        error: null
-      
-    });
 
     axiosWithAuth()
     .put(`/api/users/${userData.id}`, userData)
     .then(response => {
       dispatch({
-        type: UPDATE_USER,
-        user: response.data.user,
-        loading: false
+        type: UPDATE_USER, 
+        payload: response.data
       });
     })
     .catch(error => {
       console.log(error);
       dispatch({
-        type: UPDATE_USER_FAIL,
-        error: error.response,
-        loading: false
-        
-      });
+      type: UPDATE_USER_FAIL,
+      payload: error.response
     });
+  });
 };
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,225 +1,66 @@
 import {
     FETCH_USER,
+    FETCH_USER_SUCCESS,
+    FETCH_USER_FAIL,
+
     UPDATE_USER,
+    UPDATE_USER_FAIL
+
     DELETE_USER,
-    GET_ALL_USERS,
-    POST_FRIEND_REQUEST,
-    GET_FRIEND_REQUEST,
-    DELETE_FRIEND_REQUEST, 
-    GET_FRIENDS, 
-    ACCEPT_FRIEND_REQUEST,
-    GET_FRIEND_REQUEST_SENT
-  } from "../actions";
+  
+  } from "./actions";
   
   const initialState = {
     loading: false,
     user: {},
     error: null,
-    users: [],
-    requests: [],
-    friends: [], 
-    requestsSent: [], 
     userDeleted: false
   };
   
   export const userProfileReducer = (state = initialState, action) => {
     switch (action.type) {
-      case FETCH_USER: {
-        const error = action.payload.error;
-        const user = action.payload.user;
-        const loading = action.payload.loading;
-        if (user !== null) {
+
+      case FETCH_USER: 
+        return {
+            ...state,
+            loading: true, 
+            error: ''
+          };
+
+      case FETCH_USER_SUCCESS: 
+        return {
+            ...state,
+            user: action.payload,
+            loading: false, 
+            error: ''
+          };
+     
+      case FETCH_USER_FAIL: 
+        return {
+            ...state,
+            error: action.payload
+          };
+
+      case UPDATE_USER:
           return {
             ...state,
-            user: user,
-            loading: loading
-          };
-        } else if (error !== null) {
+            user: action.payload,
+            loading: false, 
+          }
+
+      case UPDATE_USER_FAIL:
           return {
-            ...state,
-            error: error,
-            loading: loading
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading
-          };
-        }
-      }
-      case GET_ALL_USERS: {
-        const error = action.payload.error;
-        const users = action.payload.users;
-        const loading = action.payload.loading;
-        if (users !== null) {
-          return {
-            ...state,
-            users: users,
-            loading: loading
-          };
-        } else if (error !== null) {
-          return {
-            ...state,
-            error: error,
-            loading: loading
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading
-          };
-        }
-      }
-      case UPDATE_USER: {
-        const error = action.payload.error;
-        const user = action.payload.user;
-        const loading = action.payload.loading;
-        if (user !== null) {
-          return {
-            ...state,
-            user: user,
-            loading: loading
-          };
-        } else if (error !== null) {
-          return {
-            ...state,
-            error: error,
-            loading: loading
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading
-          };
-        }
-      }
-      case DELETE_USER: {
+              ...state,
+            error: action.payload
+          }
+
+      case DELETE_USER: 
         return {
           ...state,
-          userDeleted: action.payload.userDeleted,
-          error: action.payload.error,
-          loading: action.payload.loading
+          userDeleted: action.payload,
+          error: action.payload,
+          loading: action.payload
         }
-        // if (action.payload.userDeleted) {
-        //   return {
-        //     ...state,
-        //   user: {},
-        //   error: action.payload.error,
-        //   loading: action.payload.loading
-        //   }
-        // } else {
-        //   return {
-        //     ...state,
-        //     loading: action.payload.loading
-        //   }
-        // }
-      }
-      case POST_FRIEND_REQUEST: {
-      }
-      case GET_FRIEND_REQUEST: {
-        const error = action.payload.error;
-        const requests = action.payload.requests;
-        const loading = action.payload.loading;
-        if (requests !== null) {
-          return {
-            ...state,
-            requests: requests,
-            loading: loading
-          };
-        } else if (error !== null) {
-          return {
-            ...state,
-            error: error,
-            loading: loading,
-            requests: []
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading,
-            requests: []
-          };
-        }
-      }
-      case DELETE_FRIEND_REQUEST: {
-        console.log(action.payload.deleteRequestId);
-        if (action.payload.deleteRequestId) {
-          console.log(action.payload.deleteRequestId);
-          return {
-            ...state,
-            requests: state.requests.filter(
-              request => request.id !== action.payload.deleteRequestId
-            )
-          };
-        } else {
-          return {
-            ...state
-          };
-        }
-      }
-      case GET_FRIENDS: {
-        const error = action.payload.error;
-        const friends = action.payload.friends;
-        const loading = action.payload.loading;
-        if (friends !== null) {
-          return {
-            ...state,
-            friends: friends,
-            loading: loading
-          };
-        } else if (error !== null) {
-          return {
-            ...state,
-            error: error,
-            loading: loading
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading
-          };
-        }
-      }
-      case ACCEPT_FRIEND_REQUEST: {
-        if (action.payload.acceptFriendRequestId) {
-          console.log(state);
-          console.log(action.payload.acceptFriendRequestId);
-          return {
-            ...state,
-            requests: state.requests.filter(
-              request => request.id !== action.payload.acceptFriendRequestId
-            )
-          };
-        }
-        return {
-          ...state
-        };
-      }
-      case GET_FRIEND_REQUEST_SENT: {
-        const error = action.payload.error;
-        const requestsSent = action.payload.requestsSent;
-        const loading = action.payload.loading;
-        if (requestsSent !== null) {
-          return {
-            ...state,
-            requestsSent: requestsSent,
-            loading: loading
-          };
-        } else if (error !== null) {
-          return {
-            ...state,
-            error: error,
-            loading: loading
-          };
-        } else {
-          return {
-            ...state,
-            loading: loading
-          };
-        }
-      }
-      default:
-        return state;
-    }
-  };
-  
+        
+  }
+}
