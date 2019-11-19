bd39d1ca7b6fd901e6bc62dec404f36b5aff9f12
Young Won <youngw417@gmail.com>
2019-11-19 07:48:28 -0800

    log-in form added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+
+
+    return (
+        <div className="header"> 
+            <h1>Droom for dream jobs</h1>
+        </div>
+
+
+
+    )
+}
---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default function Welcome() {
+
+
+
+    return(
+        <div className="welcome">
+            <h1>Welcome to Droom</h1>
+            <h2>Search for your dream jobs</h2>
+
+            <img src="https://images.unsplash.com/photo-1525876183281-0d0d9308010d?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=525&q=60" alt="droom"></img>
+
+        </div>
+
+    )
+}
