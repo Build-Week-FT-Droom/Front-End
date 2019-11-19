0f752cf4793406f9dcc7900a297406b7efe9a546
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-19 11:17:01 -0500

    added company form, job form to comp profile

---
+++ src/components/CompanyProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CompanyForm from '../forms/CompanyForm';
+import CompanyJobForm from '../forms/CompanyJobForm';
+
+function CompanyProfile() {
+    return (
+        <div>
+            <CompanyForm/>
+            <CompanyJobForm/>
+
+        </div>  
+
+
+    )
+}
+
+export default CompanyProfile;
